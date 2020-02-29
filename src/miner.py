7a140b696737231ceee454a72255a836fac76783
CangCiwei <61597699+CangCiwei@users.noreply.github.com>
2020-02-29 12:21:49 +0300

    Добавлено извлечение токена и извлечение данных

---
+++ src/miner.py
@@ -0,0 +1,18 @@
+import requests
+
+def read_token(path):
+    """ извлечение токена """
+    with open(path) as fd:
+        token = fd.read()
+    return token
+
+def load_data(url, token):
+    """ извлечение данных """
+    data = None
+    try:
+        res=requests.get(url, headers={"X-Auth-Token": token})
+        if res.status_code == 200:
+            data = res.json()
+    except:
+        pass
+    return data
