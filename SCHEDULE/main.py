46b2a335900e675c6e4961e50db44108ef6a7658
CangCiwei <61597699+CangCiwei@users.noreply.github.com>
2020-03-15 15:16:31 +0300

    Update main.py

--- SCHEDULE/main.py
+++ SCHEDULE/main.py
@@ -1,68 +1,59 @@
 import cmd
 import storage
 
-days = {'mon': 1 , 'tue': 2, 'etc...': 'etc...'}
+week = {'Пн': 1 , 'Вт': 2, 'Ср': 3, 'Чт': 4, 'Пт': 5, 'Сб': 6, 'Вс': 7}
 
-error_message = 'er' #сообщение об ошибке, могут быть разными
-
-def prepare_schedule(data):
-    """Преобразует набранное дневное расписание в словарь вида {1: 'Матматика', 2: 'Русский язык, '}"""
-    pass
+error_message = 'Ошибка' 
 
 class Cli(cmd.Cmd):
 
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = '> '
-        self.intro = "Приветствие" 
-        self.doc_header = "Доступные команды 'help _команда_'"
+        self.intro = "Здравствуйте, для ознакомления с функционалом: help" 
+        self.doc_header = "Доступные команды, для пояснения: help <название_команды>"
 
     def do_get_schedule(self, args):
-        """Описание"""
+        """Вывод расписания:
+            1) аргумент = '' - все расписание
+            2) аргумент = день недели в формате: 'пн' - конкретный день"""
         if args == '':
             print(storage.get_schedule())
-        elif args in map(str, range(1, 8)):
-            print(storage.get_schedule(int(args)))
+        elif args in week:
+            print(storage.get_schedule(week[args]))
         else:
-            print(error_message)
+            print("Ошибка ввода")
 
     def do_change_schedule(self, args):
-        """Описание"""
-        if len(args.split()) > 1:
-            day = args.split()[0]
-            if days.get(day):
-                schedule = prepare_schedule(args[1:])
-                storage.change_schedule(days.get(day), schedule)
-            else:
-                print(error_message)
+        """Изменение расписания, формат: <День недели>, <Урок 1>, <Урок 2> и т.д."""
+        args = args.split()
+        if args[0] in week.keys():
+            day = week[args[0]]
+            storage.change_schedule(day, args[1:])
         else:
-            print(error_message)
+            print("Ошибка ввода")
 
     def do_add_note(self, args):
-        """Описание"""
-        if len(args.split()) > 2:
-            day = args.split()[0]
-            event = args.split()[1]
-            note = args.split()[2:]
-            if days.get(day) and event.isdigit():
-                storage.add_note(days.get(day), int(event), note)
-            else:
-                print(error_message)
+        """Добавление заметки, формат: <day> <event> <note>"""
+        args = args.split()
+        if len(args) > 2 and args[0] in week.keys():
+            day = week[args[0]]
+            event = args[1]
+            note = args[2:]
+            storage.add_note(day, event, note)
         else:
-            print(error_message)
+            print("Ошибка ввода")
 
     def do_add_todo(self, args):
-        """Описание"""
-        if len(args.split()) > 2:
-            day = args.split()[0]
-            event = args.split()[1]
-            note = args.split()[2:]
-            if days.get(day) and event.isdigit():
-                storage.add_note(days.get(day), int(event), note, todo=True)
-            else:
-                print(error_message)
+        """Добавление домашнего задания, формат: <День> <Предмет> <Д/З> is_done"""
+        args = args.split()
+        if len(args) > 2 and args[0] in week.keys():
+            day = week[args[0]]
+            event = args[1]
+            note = args[2:]
+            storage.add_note(day, event, note, todo=True)
         else:
-            print(error_message)
+            print("Ошибка ввода")
 
     def do_mark_as_done(self, args):
         if len(args.split()) == 3:
@@ -77,27 +68,26 @@ class Cli(cmd.Cmd):
             print(error_message)
 
     def do_get_note(self, args):
-        if len(args.split()) == 3:
-            day = args.split()[0]
-            event = args.split()[1]
-            note_num = args.split()[2]
-            if days.get(day) and event.isdigit() and note_num.isdigit():
-                storage.get_note(days.get(day), int(event), int(note_num))
-            else:
-                print(error_message)
+        args = args.split()
+        if len(args) == 3 and args[0] in week.keys():
+            day = week[args[0]]
+            event = args[1]
+            note_num = args[2]
+            storage.get_note(day, event, int(note_num))
         else:
-            print(error_message)
+            print("Ошибка ввода")
+    
+    def do_get_all_todo(self, a):
+        for idd, note in enumerate(storage.get_all_notes()):
+            print(n)
+        
 
-    def do_exit(self, args):
-        """Описание"""
-        exit()
-
-    def do_q(self, args):
-        """Описание"""
+    def do_exit(self, strr):
+        """Выход"""
         exit()
 
     def default(self, line):
-        print(error_message)
+        print("Ошибка ввода")
 
 if __name__ == "__main__":
     cli = Cli()
